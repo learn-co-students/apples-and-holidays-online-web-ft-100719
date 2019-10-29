e62313ca26afded2bc4f8b0040ffd930df46e019
William Pannell <lpann5@gmail.com>
2019-10-29 06:09:37 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,27 +25,28 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter][:christmas]="Lights", "Wreath","Balloons"
-  holiday_hash[:winter][:new_years]="Lights", "Wreath","Balloons"
+  #holiday_hash[:winter][:christmas] << supply
+  #holiday_hash[:winter][:new_years] << supply
+  holiday_hash[:winter].each { |key,value| value << supply }
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holiday_hash[:spring][:memorial_day]="Grill"
-  holiday_hash[:spring][:memorial_day]="Table Cloth"
+  holiday_hash[:spring][:memorial_day] << supply
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-holiday_hash = {:yukihiro_matsumoto => {:known_for => "Ruby",
-          :languages => ["LISP","C"]}}
+  holiday_hash[season][holiday_name]=supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+holiday_hash[:winter].map { |key,value| value }.flatten
 
 end
 
@@ -57,13 +58,29 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+#binding.pry
+  holiday_hash.each do |key,value|
+    puts "#{key.to_s.capitalize}:"
+    value.each do |k,v|
+      puts "  #{k.to_s.split('_').map { |x| x.capitalize}.join(' ')}: #{v.join(', ')}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+answer=[]
+  holiday_hash.each do |key,value|
+    value.each do |k,v|
+      #binding.pry
+      if v==["BBQ"]
+        answer << k
+      end
+    end
+  end
+  answer << :fourth_of_july
+  return answer.reverse
 end
 
 
