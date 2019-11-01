fb4edafc627746859b1ff789afbc9b0d83bd3889
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-01 17:44:30 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,11 +58,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season, event|
     if season == :winter
-      return event.values
+      return event.values.flatten
     end
   end
 end
